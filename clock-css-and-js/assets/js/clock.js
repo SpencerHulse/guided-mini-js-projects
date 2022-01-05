2973d3fe155657576ee66aec95b44a5f76970582
Spencer Hulse <hulse.spencer@gmail.com>
2022-01-05 16:14:32 -0500

    clock-css-and-js fixed an bug when the deg rolls over

--- clock-css-and-js/assets/js/clock.js
+++ clock-css-and-js/assets/js/clock.js
@@ -1,4 +1,3 @@
-const hand = document.querySelector(".hand");
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
@@ -11,16 +10,36 @@ let setDate = () => {
   //converts the seconds to the degree for rotation
   //The + 90 offsets the initial 90 degree rotation in css
   const secondsDegrees = (seconds / 60) * 360 + 90;
+  //adding a no-transition class to override the transition and avoid a bug when deg rolls over
+  if (secondsDegrees == 90) {
+    secondHand.classList.add("no-transition");
+  } else {
+    secondHand.classList.remove("no-transition");
+  }
   //applies the transform using the current second and degree
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
   const minutesDegrees = (minutes / 60) * 360 + 90;
+
+  if (secondsDegrees == 90) {
+    minuteHand.classList.add("no-transition");
+  } else {
+    minuteHand.classList.remove("no-transition");
+  }
+
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
   //12 instead of 60 due to only having 12 hours
   const hoursDegrees = (hours / 12) * 360 + 90;
+
+  if (secondsDegrees == 90) {
+    hourHand.classList.add("no-transition");
+  } else {
+    hourHand.classList.remove("no-transition");
+  }
+
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 };
 //set immediately to prevent the hands from being stationary upon first load
