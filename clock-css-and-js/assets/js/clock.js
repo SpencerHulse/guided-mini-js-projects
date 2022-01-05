565f85e19cd5b9026f58c3a73a9e088d5e91b39a
Spencer Hulse <hulse.spencer@gmail.com>
2022-01-05 15:56:41 -0500

    clock-css-and-js logic for ticking hands

---
+++ clock-css-and-js/assets/js/clock.js
@@ -0,0 +1,29 @@
+const hand = document.querySelector(".hand");
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+let setDate = () => {
+  //gets the current date/time
+  const now = new Date();
+  //gets the current seconds every second due to interval
+  const seconds = now.getSeconds();
+  //converts the seconds to the degree for rotation
+  //The + 90 offsets the initial 90 degree rotation in css
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  //applies the transform using the current second and degree
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  //12 instead of 60 due to only having 12 hours
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+};
+//set immediately to prevent the hands from being stationary upon first load
+setDate();
+//updates every second after initial load
+setInterval(setDate, 1000);
